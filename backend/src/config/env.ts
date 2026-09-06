a0f59620412d403f4af07436a8b61e2209be09af
Kirtanyadav199 <kirtanyadav24@gmail.com>
2026-09-06 09:16:06 +0530

    feat: setup MongoDB connection with mongoose

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -5,4 +5,5 @@ dotenv.config();
 export const env = {
   port: process.env.PORT || 5000,
   nodeEnv: process.env.NODE_ENV || "development",
+  mongoUri: process.env.MONGO_URI || "",
 };
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
 import { env } from "./config/env";
+import { connectDB } from "./config/db";
+import { log } from "console";
 
 
 const app = express();
@@ -8,7 +10,12 @@ app.get("/", (req: Request, res: Response) => {
   res.json({ message: "Server is running" });
 });
 
-app.listen(env.port, () => {
-  console.log(`Server running on http://localhost:${env.port} [${env.nodeEnv}]`);
-});
+const startServer = async()=>{
+  await connectDB();
+  app.listen(env.port,()=>{
+    console.log(`Server running on http://localhost:${env.port} [${env.nodeEnv}]`);
+    
+  })
+}
 
+startServer();
